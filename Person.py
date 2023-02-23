ad4a1313553afe8f677f9c1803e6372c92ddf8bd
Sjfke <sjfke.pool.shark@hotmail.com>
2023-02-23 16:28:07 +0100

    Person add PyCharm docstrings

--- Person.py
+++ Person.py
@@ -18,15 +18,39 @@ class Person:
         self.__uuid = str(uuid.uuid4())
 
     def get_name(self):
+        """
+        Getter Name
+        :return: name of person
+        :rtype: str
+        """
         return self.__name
 
     def set_name(self, value):
+        """
+        Setter name
+        :param value: name of person
+        :type value: str
+        :return: None
+        :rtype: NoneType
+        """
         self.__name = value
 
     def get_age(self):
+        """
+        Getter age
+        :return: age of person
+        :rtype: int
+        """
         return self.__age
 
     def set_age(self, value):
+        """
+        Setter age
+        :param value: age of person
+        :type value: int
+        :return: None
+        :rtype: NoneType
+        """
         if not isinstance(value, int):
             raise ValueError(f"invalid int for age: '{value}'")
         elif value > 0:
@@ -35,16 +59,37 @@ class Person:
             self.__age = 0
 
     def get_sex(self):
+        """
+        Getter gender
+        :return: gender of person
+        :rtype: str
+        """
         return self.__sex
 
     def set_sex(self, value):
+        """
+        Setter gender
+        :param value: gender of person ('M', 'F', 'N')
+        :type value: str
+        :return: None
+        :rtype: NoneType
+        """
         self.__sex = value
 
     def get_uuid(self):
+        """
+        Getter uuid
+        :return:UUID value
+        :rtype: str
+        """
         return self.__uuid
 
     def __str__(self):
-        """ String representation """
+        """
+        String representation
+        :return: human readable representation
+        :rtype: str
+        """
         __str = 'Person: '
         __str += str(self.__name) + ', '
         __str += str(self.__age) + ', '
@@ -53,7 +98,11 @@ class Person:
         return __str
 
     def __repr__(self):
-        """ repr() string representation """
+        """
+        repr() string representation
+        :return: programmtic representation
+        :rtype: str
+        """
         __str = "{"
         __str += f"'name': {self.__name}, "
         __str += f"'age': {self.__age}, "
