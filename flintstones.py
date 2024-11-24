676428e49a7d7341eea0ded25e7f2197be7c01a4
Geoffrey Collis <geoffreycollis@hotmail.com>
2024-11-24 20:08:09 +0100

    add python type hints

--- flintstones.py
+++ flintstones.py
@@ -2,50 +2,40 @@ import argparse
 import sys
 
 # https://docs.python.org/3/howto/argparse.html
-
 _dict = {'Fred': 30, 'Wilma': 25, 'Pebbles': 1, 'Dino': 5}
 
 
-def get_names():
+def get_names() -> list[str]:
     """
     Get Flintstones family firstnames
-
-    :return: Flintstones family firstnames
-    :rtype: list
+    :return: list of names
     """
-    return _dict.keys()
+    return list(_dict.keys())
 
 
-def get_ages():
+def get_ages() -> list[int]:
     """
     Get Flintstones family ages
-
-    :return: Flintstones family ages
-    :rtype: list
+    :return: list of ages
     """
+    return list(_dict.values())
 
-    return _dict.values()
 
-
-def get_person(name=None):
+def get_person(name: str = None) -> (dict[str,int] | None):
     """
-    Get age of person
-
-    :param name: of the person
-    :type name: str
-
-    :rtype: str
-    :return: firstnames or None
+    Get age of Flintstones family member
+    :param name: firstname
+    :return: integer age, KeyError or None
     """
-
     if name is not None:
 
         try:
             _ans = {name: _dict[name]}
             return _ans
         except KeyError:
-            return f"KeyError: {name}"
-            # return "KeyError: {0}".format(name)  # prior to Python 3.6
+            print(f"KeyError: {name} not found", file=sys.stderr)
+            # print("KeyError: {0} not found".format(name))  # prior to Python 3.6
+            return None
     else:
         return None
 
@@ -55,19 +45,21 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Simple Command Line Application')
     parser.add_argument('-n', '--names', action='store_true', default=False, help='display names')
     parser.add_argument('-a', '--ages', action='store_true', default=False, help='display ages')
-    parser.add_argument('-p', '--person', type=str, default=None, help='display person')
+    parser.add_argument('-p', '--person', type=str, default=None, help='display person age')
     parser.add_argument('-v', '--verbose', action='count', default=0)
 
     args = parser.parse_args()
 
     if args.verbose >= 1:
-        print("args: {0}".format(args.__str__()))
+        print(f"args: {args.__str__()}")
 
     if args.names:
-        print("{0}".format(get_names()))
+        print(f"{get_names()}")
     elif args.ages:
-        print("{0}".format(get_ages()))
+        print(f"{get_ages()}")
     elif args.person:
-        print("{0}".format(get_person(name=args.person)))
+        print(f"{get_person(name=args.person)}")
+    else:
+        parser.print_help()
 
     sys.exit(0)
